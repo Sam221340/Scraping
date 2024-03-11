aaffda40cd08b9298954a5d3c27bc5b08d4970a2
Rohit <rt23707@gmail.com>
2024-03-11 18:39:48 +0530

    d

--- scraping/all_reviews.py
+++ scraping/all_reviews.py
@@ -13,36 +13,51 @@ t_posted = []
 
 
 driver = webdriver.Chrome()
-url = "https://www.flipkart.com/shivark-new-designer-set-3-black-dial-watch-1-bracelet-boys-analog-men/product-reviews/itm097ce701fe44a?pid=WATGNH9QQKC7JZEY&lid=LSTWATGNH9QQKC7JZEYKCULEP&marketplace=FLIPKART"
-driver.get(url)
-driver.maximize_window()
-soup = BeautifulSoup(driver.page_source,"lxml")
-sleep(3)
+for i in range(1,4):
+    url = "https://www.flipkart.com/shivark-new-designer-set-3-black-dial-watch-1-bracelet-boys-analog-men/product-reviews/itm097ce701fe44a?pid=WATGNH9QQKC7JZEY&lid=LSTWATGNH9QQKC7JZEYKCULEP&marketplace=FLIPKART&page="+str(i)
+    driver.get(url)
+    driver.maximize_window()
+    soup = BeautifulSoup(driver.page_source,"lxml")
+    sleep(4)
 
-parent_div = soup.find_all("div",class_="row _1ExUpQ")
+    parent_div = soup.find_all("div",class_="row _1ExUpQ")
 
-
-customer_name = soup.find_all("p",class_="_2sc7ZR _2V5EHH _1QgsS5")
-customer_rating = soup.find_all("div",class_="_3LWZlK _1BLPMq _3B8WaH")
-customer_review = soup.find_all("div",class_="_6K-7Co")
-print(soup.select_one("div p:nth-of-type(2)").text)
-
-for div in parent_div:
-    time_posted = div.find_all("p")
-    t_posted.append(time_posted[1].text)
-
-for name,rating,review in zip(customer_name,customer_rating,customer_review):
-    c_name.append(name.text)
-    c_rating.append(rating.text)
-    c_review.append(review.text)
+    try:
+        customer_name = soup.find_all("p",class_="_2sc7ZR _2V5EHH _1QgsS5")
+        customer_rating = soup.find_all("div",class_="_3LWZlK _1BLPMq _3B8WaH")
+        customer_review = soup.find_all("div",class_="_6K-7Co")
+        if customer_review == None:
+            customer_review
 
 
-print(c_name)
-print(c_rating)
-print(c_review)
-print(t_posted)
+
+        for div in parent_div:
+            time_posted = div.find_all("p")
+            t_posted.append(time_posted[1].text)
+
+        for name,rating,review in zip(customer_name,customer_rating,customer_review):
+            c_name.append(name.text)
+            c_rating.append(rating.text)
+            c_review.append(review.text)
+    except:
+        print("Not found")
+
+
+print(len(c_name))
+print(len(c_rating))
+print(len(c_review))
+print(len(t_posted))
+print((c_name))
+print((c_rating))
+print((c_review))
+print((t_posted))
 new_list = t_posted[1::2]
+t_posted.pop()
+print(len(t_posted))
 
-print(new_list)
+
+df = pd.DataFrame({"Customer":c_name,"Rating":c_rating,"Review":c_review,"Posted_in":t_posted})
+df.to_excel("Full_reviews.xlsx")
+# print(new_list)
 
 sleep(4)
--- web_tables.py
+++ web_tables.py
@@ -1,40 +1,17 @@
+import pandas as pd
+from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver import ActionChains
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from time import sleep
 
 driver = webdriver.Chrome()
-driver.get("https://demoqa.com/")
+url = "https://www.amazon.in/s?k=watches+for+men&crid=5Y76Y4O5WVXO&sprefix=wa%2Caps%2C815&ref=nb_sb_ss_ts-doa-p_3_2"
+driver.get(url)
 sleep(2)
-elem = driver.get("https://demoqa.com/elements")
-sleep(2)
-lista = ['firstName','lastName']
-web_tables = driver.find_element(By.ID,'item-3')
-web_tables.click()
-driver.execute_script("window.scrollTo(0,400)")
-sleep(4)
-add_record = driver.find_element(By.ID,'addNewRecordButton')
-add_record.click()
-sleep(3)
-for i in lista:
-    fname = driver.find_element(By.ID,i)
-    fname.send_keys('Rohit')
-email = driver.find_element(By.ID,'userEmail')
-email.send_keys('test1@gmail.com')
-age = driver.find_element(By.ID,'age')
-age.send_keys(33)
-salary = driver.find_element(By.ID,'salary')
-salary.send_keys(22000)
-department = driver.find_element(By.ID,'department')
-department.send_keys('IT')
-submit = driver.find_element(By.ID,'submit')
-submit.click()
-sleep(5)
-search = driver.find_element(By.ID,'searchBox')
-search.send_keys('hit')
-search.send_keys(Keys.RETURN)
-sleep(5)
 
-x = "hell"
-y = "o"
-print("hell"+y)
+click_element = driver.find_element(By.XPATH,'//*[@id="search"]/div[1]/div[1]/div/span[1]/div[1]/div[7]/div/div/span/div/div/div[1]/div/span/a')
+click_element.click()
+sleep(4)
+driver.quit()
